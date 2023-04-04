f862952f9d18064a229926000b1228000e9dac90
tokkissi <alal4674@gmail.com>
2023-04-04 23:16:06 +0900

    Feat: add base layout (header, footer)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-900 text-white py-2 text-sm text-center">
+      Code your life | Ⓒ All Right Reserved
+    </footer>
+  );
+}
