f8fcc99102246acd9c6f886fade432a48284c93f
tokkissi <alal4674@gmail.com>
2023-04-05 09:45:11 +0900

    Feat: add get posts functions

---
+++ src/service/posts.ts
@@ -0,0 +1,28 @@
+import { readFile } from "fs/promises";
+import path from "path";
+
+export type Post = {
+  title: string;
+  description: string;
+  date: Date;
+  category: string;
+  path: string;
+  featured: boolean;
+};
+
+export async function getAllPosts() {
+  const filePath = path.join(process.cwd(), "data", "posts.json");
+  return readFile(filePath, "utf-8")
+    .then<Post[]>(JSON.parse)
+    .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getFeaturedPosts() {
+  const allPosts = await getAllPosts();
+  return allPosts.filter((post) => post.featured);
+}
+
+export async function getNonFeaturedPosts() {
+  const allPosts = await getAllPosts();
+  return allPosts.filter((post) => !post.featured);
+}
