06d820a06a1f48b33149a9f4fa0dc3ecd52f9b47
tokkissi <alal4674@gmail.com>
2023-04-07 12:14:13 +0900

    Feat: add aboutPage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,28 @@
+import HeroProfile from "@/components/HeroProfile";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
 export default function AboutPage() {
-  return <p>about page</p>;
+  return (
+    <>
+      <HeroProfile />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center rounded-2xl max-w-xl mx-auto">
+        <h2 className={TITLE_CLASS}>Who am I?</h2>
+        <p className="mb-8">
+          삶의 문제를 코딩해나가는 프론트엔드 개발자
+          <br />
+          코딩하는 토끼, 토끼씨
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          React, TypeScript, Node.js,
+          <br />
+          Redux, React Query, Styled Component,
+          <br />
+          Tailwind CSS, MongoDB,
+          <br />
+          Next.js, Git, VS Code
+        </p>
+      </section>
+    </>
+  );
 }
